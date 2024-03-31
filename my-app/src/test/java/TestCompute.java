cc967c5e2a8a1936aa3433d4b6bdd8975dfc2150
kbekleyen <kaanmbekleyen@gmail.com>
2024-03-31 15:57:45 +0300

    Unit Tests For Compute.java
    Tests all branches and instructions

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestCompute {
   Compute c;
@@ -14,4 +15,35 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+
+  @Test
+  public void testOccurrences_Empty() {
+    MessageQueue mq = mock(MessageQueue.class);
+    Mockito.when(mq.size()).thenReturn(0);
+    c = new Compute(mq);
+    assertEquals(c.countNumberOfOccurrences(""),-1);
+  }
+
+  @Test
+  public void testOccurrences_NoOccurrence() {
+    MessageQueue mq = mock(MessageQueue.class);
+    Mockito.when(mq.size()).thenReturn(3);
+    Mockito.when(mq.contains("test1")).thenReturn(false);
+    c = new Compute(mq);
+    assertEquals(c.countNumberOfOccurrences("test1"),0);
+  }
+
+  @Test
+  public void testOccurrences() {
+    MessageQueue mq = mock(MessageQueue.class);
+    Mockito.when(mq.size()).thenReturn(3);
+    Mockito.when(mq.contains("test1")).thenReturn(true);
+    Mockito.when(mq.getAt(0)).thenReturn("test0");
+    Mockito.when(mq.getAt(1)).thenReturn("test1");
+    Mockito.when(mq.getAt(2)).thenReturn("test2");
+    c = new Compute(mq);
+    assertEquals(c.countNumberOfOccurrences("test1"),1);
+  }
+
+  
 }
--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,9 +1,12 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TestUtil {
   Util c;
@@ -12,5 +15,26 @@ public class TestUtil {
   public void setUp() { c = new Util(); }
 
   @Test
-  public void example() { assertTrue(true); }
+  public void testSingleArgument() { 
+    assertFalse(c.compute(1)); 
+  }
+  public void testEvenNumberOfArguments() { 
+    assertFalse(c.compute(1,2)); 
+  }
+
+  @Rule
+  public ExpectedException thrown= ExpectedException.none();
+
+  public void testZeroAsArgument() {
+    thrown.expect(RuntimeException.class); 
+    c.compute(0,1,2); 
+  }
+
+  public void testZeroSum() {
+    assertTrue(c.compute(-3,1,2)); 
+  }
+
+  public void testEndOfMethod() {
+    assertTrue(c.compute(1,2,3)); 
+  }
 }
